693873e96dfff30151fca19008dd7ac338573587
zhengshugan <zhengshugan@foxmail.com>
2023-06-02 19:49:53 +0800

    chore: init project

---
+++ easypan/src/main/java/com/easypan/EasyPanApplication.java
@@ -0,0 +1,11 @@
+package com.easypan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyPanApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(EasyPanApplication.class, args);
+  }
+}
